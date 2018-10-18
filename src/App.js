051cd441a70c77c02dde0cb6eb2b57d22d8e2df0
joepal <joe.palomino50@gmail.com>
2018-10-18 11:31:34 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { css as emoCSS } from "emotion";
 
+const css = (...args) => ({ className: emoCSS(...args) });
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        {...css({
+          border: "solid 6px rgba(0,0,0,0.84)",
+          padding: "2rem",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column"
+        })}
+      >
+        <h1 {...css({ fontSize: "3rem" })}>Render Cake</h1>
+        <p {...css({ fontFamily: "'Open Sans'", fontStyle: "italic" })}>
+          something wicked this way comes
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { injectGlobal } from "emotion";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+injectGlobal`
+    body {
+        font-family: 'Pacifico', cursive;
+        font-size: 16px;
+        color: rgba(0,0,0,0.84);
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+`;
